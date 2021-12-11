78a3480cdeaaece5159d13d5c8fedde105dc9e9a
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-12-11 16:24:56 +0100

    Implemented the complete visualization

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
 import pathlib
 import re
+import json
+import random
 
 import dash
 from dash import html
 from dash import dcc
 import pandas as pd
 from dash.dependencies import Input, Output, State
-import cufflinks as cf
+from colormap import rgb2hex
+
+r = lambda: random.randint(0, 255)
 
 # Initialize app
-
 app = dash.Dash(
     __name__,
     meta_tags=[
@@ -43,50 +46,12 @@ df_full_data["County"] = (
         df_full_data["Unnamed: 0"] + ", " + df_full_data.County.map(str)
 )
 
-YEARS = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]
-
-BINS = [
-    "0-2",
-    "2.1-4",
-    "4.1-6",
-    "6.1-8",
-    "8.1-10",
-    "10.1-12",
-    "12.1-14",
-    "14.1-16",
-    "16.1-18",
-    "18.1-20",
-    "20.1-22",
-    "22.1-24",
-    "24.1-26",
-    "26.1-28",
-    "28.1-30",
-    ">30",
-]
-
-DEFAULT_COLORSCALE = [
-    "#f2fffb",
-    "#bbffeb",
-    "#98ffe0",
-    "#79ffd6",
-    "#6df0c8",
-    "#69e7c0",
-    "#59dab2",
-    "#45d0a5",
-    "#31c194",
-    "#2bb489",
-    "#25a27b",
-    "#1e906d",
-    "#188463",
-    "#157658",
-    "#11684d",
-    "#10523e",
-]
+YEARS = [2016, 2017, 2018, 2019, 2020, 2021]
 
 DEFAULT_OPACITY = 0.8
 
 mapbox_access_token = "pk.eyJ1IjoicnZkaG9vcm4iLCJhIjoiY2t3eGVtbGNrMGRwNzJ3bnJucWp4emoweiJ9.5P0vCt-6KnIubabETLsymA"
-mapbox_style = "mapbox://styles/rvdhoorn/ckwxew4um4opy14n2i7puhx6k"
+mapbox_style = "mapbox://styles/rvdhoorn/ckx1u7m3v3k0o14pam2buej1s"
 
 # App layout
 app.layout = html.Div(
@@ -95,19 +60,7 @@ app.layout = html.Div(
         html.Div(
             id="header",
             children=[
-                html.A(
-                    html.Img(id="logo", src=app.get_asset_url("dash-logo.png")),
-                    href="https://plotly.com/dash/",
-                ),
-                html.A(
-                    html.Button("Enterprise Demo", className="link-button"),
-                    href="https://plotly.com/get-demo/",
-                ),
-                html.A(
-                    html.Button("Source Code", className="link-button"),
-                    href="https://github.com/plotly/dash-sample-apps/tree/main/apps/dash-opioid-epidemic",
-                ),
-                html.H4(children="Rate of US Poison-Induced Deaths"),
+                html.H4(children="Car accident visualization"),
             ],
         ),
         html.Div(
@@ -130,8 +83,8 @@ app.layout = html.Div(
                                     value=min(YEARS),
                                     marks={
                                         str(year): {
-                                            "label": str(year),
-                                            "style": {"color": "#7fafdf"},
+                                            "label": "sum" if year == 2021 else str(year),
+                                            "style": {"color": "#000"},
                                         }
                                         for year in YEARS
                                     },
@@ -198,8 +151,8 @@ app.layout = html.Div(
                             figure=dict(
                                 data=[dict(x=0, y=0)],
                                 layout=dict(
-                                    paper_bgcolor="#F4F4F8",
-                                    plot_bgcolor="#F4F4F8",
+                                    paper_bgcolor=(227, 227, 227),
+                                    plot_bgcolor=(227, 227, 227),
                                     autofill=True,
                                     margin=dict(t=75, r=50, b=100, l=50),
                                 ),
@@ -213,14 +166,44 @@ app.layout = html.Div(
 )
 
 
-
 @app.callback(
     Output("county-choropleth", "figure"),
     [Input("years-slider", "value")],
     [State("county-choropleth", "figure")],
 )
 def display_map(year, figure):
-    cm = dict(zip(BINS, DEFAULT_COLORSCALE))
+    variable_name = "number_of_accidents_" + str(year)
+
+    if year == 2021:
+        variable_name = "number_of_accidents"
+
+    with open("./assets/data/lad.json") as infile:
+        json_data = json.load(infile)
+
+    def color(x, max):
+        max_val = 255
+        if x <= 0:
+            return rgb2hex(191, 191, 191)
+
+        val = int((x / max) * max_val)
+
+        return rgb2hex(255, max_val - val, max_val - val)
+
+    accidents_per_100000 = []
+    names = ["number_of_accidents_2016", "number_of_accidents_2017", "number_of_accidents_2018",
+             "number_of_accidents_2019", "number_of_accidents_2020"]
+    if variable_name in names:
+        for name in names:
+            for feature in json_data['features']:
+                accidents_per_100000.append(
+                    int(feature['properties'][name] / feature['properties']['population'] * 100000))
+
+    else:
+        for feature in json_data['features']:
+            accidents_per_100000.append(
+                int(feature['properties'][variable_name] / feature['properties']['population'] * 100000))
+
+    maximum = max(accidents_per_100000)
 
     data = [
         dict(
@@ -237,28 +220,28 @@ def display_map(year, figure):
         dict(
             showarrow=False,
             align="right",
-            text="<b>Age-adjusted death rate<br>per county per year</b>",
-            font=dict(color="#2cfec1"),
-            bgcolor="#1f2630",
+            text="<b>Number of car accidents<br>per 100.000 people</b>",
+            font=dict(color="#000000"),
             x=0.95,
             y=0.95,
         )
     ]
 
-    for i, bin in enumerate(reversed(BINS)):
-        color = cm[bin]
+    binsize = int(maximum / 5)
+
+    for i in range(1, maximum, binsize - 1):
+        col = color(i, maximum)
         annotations.append(
             dict(
-                arrowcolor=color,
-                text=bin,
+                arrowcolor=col,
+                text=str(i),
                 x=0.95,
-                y=0.85 - (i / 20),
+                y=0.85 - ((i / (binsize - 1)) / 20),
                 ax=-60,
                 ay=0,
                 arrowwidth=5,
                 arrowhead=0,
-                bgcolor="#1f2630",
-                font=dict(color="#2cfec1"),
+                font=dict(color="#000000"),
             )
         )
 
@@ -285,27 +268,26 @@ def display_map(year, figure):
         dragmode="lasso",
     )
 
-    geo_layer = dict(
-        sourcetype="json",
-        source="https://http://127.0.0.1:8050/assets/data/lad.json",
-        type="fill",
-        color="#25a27b",
-        opacity=DEFAULT_OPACITY,
-        # CHANGE THIS
-        fill=dict(outlinecolor="#afafaf"),
-    )
+    for feature in json_data['features']:
+        geo_layer = dict(
+            sourcetype="geojson",
+            source=feature,
+            type="fill",
+            color=color(feature['properties'][variable_name] / feature['properties']['population'] * 100000,
+                        maximum),
+            opacity=DEFAULT_OPACITY,
+            fill=dict(outlinecolor="#afafaf")
+        )
 
-    layout["mapbox"]["layers"].append(geo_layer)
+        layout["mapbox"]["layers"].append(geo_layer)
 
     fig = dict(data=data, layout=layout)
     return fig
 
 
-
 @app.callback(Output("heatmap-title", "children"), [Input("years-slider", "value")])
 def update_map_title(year):
-    return "Heatmap of age adjusted mortality rates \
-				from poisonings in year {0}".format(
+    return "Heatmap of population adjusted accident rate per district for {0}".format(
         year
     )
 
@@ -318,17 +300,15 @@ def update_map_title(year):
         Input("years-slider", "value"),
     ],
 )
-
 def display_selected_data(selectedData, chart_dropdown, year):
-
     if selectedData is None:
         return dict(
             data=[dict(x=0, y=0)],
             layout=dict(
                 title="Click-drag on the map to select counties",
-                paper_bgcolor="#1f2630",
-                plot_bgcolor="#1f2630",
-                font=dict(color="#2cfec1"),
+                paper_bgcolor=(227, 227, 227),
+                plot_bgcolor=(227, 227, 227),
+                font=dict(color="#000000"),
                 margin=dict(t=75, r=50, b=100, l=75),
             ),
         )
@@ -368,12 +348,12 @@ def display_selected_data(selectedData, chart_dropdown, year):
         fig_data = fig["data"]
 
         fig_data[0]["text"] = deaths_or_rate_by_fips.values.tolist()
-        fig_data[0]["marker"]["color"] = "#2cfec1"
+        fig_data[0]["marker"]["color"] = (227, 227, 227)
         fig_data[0]["marker"]["opacity"] = 1
         fig_data[0]["marker"]["line"]["width"] = 0
         fig_data[0]["textposition"] = "outside"
-        fig_layout["paper_bgcolor"] = "#1f2630"
-        fig_layout["plot_bgcolor"] = "#1f2630"
+        fig_layout["paper_bgcolor"] = (227, 227, 227)
+        fig_layout["plot_bgcolor"] = (227, 227, 227)
         fig_layout["font"]["color"] = "#2cfec1"
         fig_layout["title"]["font"]["color"] = "#2cfec1"
         fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
