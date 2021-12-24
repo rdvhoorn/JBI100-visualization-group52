d788f0db03319ebcd4a3aea66c3ac3b2c820def1
Robin van Hoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-12-24 10:52:40 +0100

    Endemic visualization (#2)
    * First adaptations to the endemic template
    
    Data is not yet working
    
    * Visualization with county borders in UK!
    
    * Implemented the complete visualization
    
    * Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,433 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+import os
+import pathlib
+import re
+import json
+import random
 
 import dash
-from dash import dcc
 from dash import html
-import plotly.express as px
+from dash import dcc
 import pandas as pd
+from dash.dependencies import Input, Output, State
+from colormap import rgb2hex
 
-app = dash.Dash(__name__)
+r = lambda: random.randint(0, 255)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [18, 1, 2, 2, 4, 18],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+# Initialize app
+app = dash.Dash(
+    __name__,
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1.0"}
+    ],
+)
+app.title = "Visualization assignment"
+server = app.server
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# Load data
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+APP_PATH = str(pathlib.Path(__file__).parent.resolve())
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+df_lat_lon = pd.read_csv(
+    os.path.join(APP_PATH, os.path.join("data", "lat_lon_counties.csv"))
+)
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+df_lat_lon["FIPS "] = df_lat_lon["FIPS "].apply(lambda x: str(x).zfill(5))
+
+df_full_data = pd.read_csv(
+    os.path.join(
+        APP_PATH, os.path.join("data", "age_adjusted_death_rate_no_quotes.csv")
     )
-])
+)
+df_full_data["County Code"] = df_full_data["County Code"].apply(
+    lambda x: str(x).zfill(5)
+)
 
-if __name__ == '__main__':
+df_full_data["County"] = (
+        df_full_data["Unnamed: 0"] + ", " + df_full_data.County.map(str)
+)
+
+YEARS = [2016, 2017, 2018, 2019, 2020, 2021]
+
+DEFAULT_OPACITY = 0.8
+
+mapbox_access_token = "pk.eyJ1IjoicnZkaG9vcm4iLCJhIjoiY2t3eGVtbGNrMGRwNzJ3bnJucWp4emoweiJ9.5P0vCt-6KnIubabETLsymA"
+mapbox_style = "mapbox://styles/rvdhoorn/ckx1u7m3v3k0o14pam2buej1s"
+
+# App layout
+app.layout = html.Div(
+    id="root",
+    children=[
+        html.Div(
+            id="header",
+            children=[
+                html.H4(children="Car accident visualization"),
+            ],
+        ),
+        html.Div(
+            id="app-container",
+            children=[
+                html.Div(
+                    id="left-column",
+                    children=[
+                        html.Div(
+                            id="slider-container",
+                            children=[
+                                html.P(
+                                    id="slider-text",
+                                    children="Drag the slider to change the year:",
+                                ),
+                                dcc.Slider(
+                                    id="years-slider",
+                                    min=min(YEARS),
+                                    max=max(YEARS),
+                                    value=min(YEARS),
+                                    marks={
+                                        str(year): {
+                                            "label": "sum" if year == 2021 else str(year),
+                                            "style": {"color": "#000"},
+                                        }
+                                        for year in YEARS
+                                    },
+                                ),
+                            ],
+                        ),
+                        html.Div(
+                            id="heatmap-container",
+                            children=[
+                                html.P(
+                                    "Heatmap",
+                                    id="heatmap-title",
+                                ),
+                                dcc.Graph(
+                                    id="county-choropleth",
+                                    figure=dict(
+                                        layout=dict(
+                                            mapbox=dict(
+                                                layers=[],
+                                                accesstoken=mapbox_access_token,
+                                                style=mapbox_style,
+                                                center=dict(
+                                                    lat=55, lon=-3.9
+                                                ),
+                                                pitch=0,
+                                                zoom=5.3,
+                                            ),
+                                            autosize=True,
+                                        ),
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+                html.Div(
+                    id="graph-container",
+                    children=[
+                        html.P(id="chart-selector", children="Select chart:"),
+                        dcc.Dropdown(
+                            options=[
+                                {
+                                    "label": "Histogram of total number of deaths (single year)",
+                                    "value": "show_absolute_deaths_single_year",
+                                },
+                                {
+                                    "label": "Histogram of total number of deaths (1999-2016)",
+                                    "value": "absolute_deaths_all_time",
+                                },
+                                {
+                                    "label": "Age-adjusted death rate (single year)",
+                                    "value": "show_death_rate_single_year",
+                                },
+                                {
+                                    "label": "Trends in age-adjusted death rate (1999-2016)",
+                                    "value": "death_rate_all_time",
+                                },
+                            ],
+                            value="show_death_rate_single_year",
+                            id="chart-dropdown",
+                        ),
+                        dcc.Graph(
+                            id="selected-data",
+                            figure=dict(
+                                data=[dict(x=0, y=0)],
+                                layout=dict(
+                                    paper_bgcolor=(227, 227, 227),
+                                    plot_bgcolor=(227, 227, 227),
+                                    autofill=True,
+                                    margin=dict(t=75, r=50, b=100, l=50),
+                                ),
+                            ),
+                        ),
+                    ],
+                ),
+            ],
+        ),
+    ],
+)
+
+
+@app.callback(
+    Output("county-choropleth", "figure"),
+    [Input("years-slider", "value")],
+    [State("county-choropleth", "figure")],
+)
+def display_map(year, figure):
+    variable_name = "number_of_accidents_" + str(year)
+
+    if year == 2021:
+        variable_name = "number_of_accidents"
+
+    with open("./assets/data/lad.json") as infile:
+        json_data = json.load(infile)
+
+    def color(x, max):
+        max_val = 255
+        if x <= 0:
+            return rgb2hex(191, 191, 191)
+
+        val = int((x / max) * max_val)
+
+        return rgb2hex(255, max_val - val, max_val - val)
+
+    accidents_per_100000 = []
+    names = ["number_of_accidents_2016", "number_of_accidents_2017", "number_of_accidents_2018",
+             "number_of_accidents_2019", "number_of_accidents_2020"]
+    if variable_name in names:
+        for name in names:
+            for feature in json_data['features']:
+                accidents_per_100000.append(
+                    int(feature['properties'][name] / feature['properties']['population'] * 100000))
+
+    else:
+        for feature in json_data['features']:
+            accidents_per_100000.append(
+                int(feature['properties'][variable_name] / feature['properties']['population'] * 100000))
+
+    maximum = max(accidents_per_100000)
+
+    data = [
+        dict(
+            lat=df_lat_lon["Latitude "],
+            lon=df_lat_lon["Longitude"],
+            text=df_lat_lon["Hover"],
+            type="scattermapbox",
+            hoverinfo="text",
+            marker=dict(size=5, color="white", opacity=0),
+        )
+    ]
+
+    annotations = [
+        dict(
+            showarrow=False,
+            align="right",
+            text="<b>Number of car accidents<br>per 100.000 people</b>",
+            font=dict(color="#000000"),
+            x=0.95,
+            y=0.95,
+        )
+    ]
+
+    binsize = int(maximum / 5)
+
+    for i in range(1, maximum, binsize - 1):
+        col = color(i, maximum)
+        annotations.append(
+            dict(
+                arrowcolor=col,
+                text=str(i),
+                x=0.95,
+                y=0.85 - ((i / (binsize - 1)) / 20),
+                ax=-60,
+                ay=0,
+                arrowwidth=5,
+                arrowhead=0,
+                font=dict(color="#000000"),
+            )
+        )
+
+    if "layout" in figure:
+        lat = figure["layout"]["mapbox"]["center"]["lat"]
+        lon = figure["layout"]["mapbox"]["center"]["lon"]
+        zoom = figure["layout"]["mapbox"]["zoom"]
+    else:
+        lat = 38.72490
+        lon = -95.61446
+        zoom = 3.5
+
+    layout = dict(
+        mapbox=dict(
+            layers=[],
+            accesstoken=mapbox_access_token,
+            style=mapbox_style,
+            center=dict(lat=lat, lon=lon),
+            zoom=zoom,
+        ),
+        hovermode="closest",
+        margin=dict(r=0, l=0, t=0, b=0),
+        annotations=annotations,
+        dragmode="lasso",
+    )
+
+    for feature in json_data['features']:
+        geo_layer = dict(
+            sourcetype="geojson",
+            source=feature,
+            type="fill",
+            color=color(feature['properties'][variable_name] / feature['properties']['population'] * 100000,
+                        maximum),
+            opacity=DEFAULT_OPACITY,
+            fill=dict(outlinecolor="#afafaf")
+        )
+
+        layout["mapbox"]["layers"].append(geo_layer)
+
+    fig = dict(data=data, layout=layout)
+    return fig
+
+
+@app.callback(Output("heatmap-title", "children"), [Input("years-slider", "value")])
+def update_map_title(year):
+    if year == 2021:
+        return "heatmap of population adjusted accident rate of last 5 years"
+
+    return "Heatmap of population adjusted accident rate per district for {0}".format(
+        year
+    )
+
+
+@app.callback(
+    Output("selected-data", "figure"),
+    [
+        Input("county-choropleth", "selectedData"),
+        Input("chart-dropdown", "value"),
+        Input("years-slider", "value"),
+    ],
+)
+def display_selected_data(selectedData, chart_dropdown, year):
+    if selectedData is None:
+        return dict(
+            data=[dict(x=0, y=0)],
+            layout=dict(
+                title="Click-drag on the map to select counties",
+                paper_bgcolor=(227, 227, 227),
+                plot_bgcolor=(227, 227, 227),
+                font=dict(color="#000000"),
+                margin=dict(t=75, r=50, b=100, l=75),
+            ),
+        )
+
+    pts = selectedData["points"]
+    fips = [str(pt["text"].split("<br>")[-1]) for pt in pts]
+    for i in range(len(fips)):
+        if len(fips[i]) == 4:
+            fips[i] = "0" + fips[i]
+    dff = df_full_data[df_full_data["County Code"].isin(fips)]
+    dff = dff.sort_values("Year")
+
+    regex_pat = re.compile(r"Unreliable", flags=re.IGNORECASE)
+    dff["Age Adjusted Rate"] = dff["Age Adjusted Rate"].replace(regex_pat, 0)
+
+    if chart_dropdown != "death_rate_all_time":
+        title = "Absolute deaths per county, <b>1999-2016</b>"
+        AGGREGATE_BY = "Deaths"
+        if "show_absolute_deaths_single_year" == chart_dropdown:
+            dff = dff[dff.Year == year]
+            title = "Absolute deaths per county, <b>{0}</b>".format(year)
+        elif "show_death_rate_single_year" == chart_dropdown:
+            dff = dff[dff.Year == year]
+            title = "Age-adjusted death rate per county, <b>{0}</b>".format(year)
+            AGGREGATE_BY = "Age Adjusted Rate"
+
+        dff[AGGREGATE_BY] = pd.to_numeric(dff[AGGREGATE_BY], errors="coerce")
+        deaths_or_rate_by_fips = dff.groupby("County")[AGGREGATE_BY].sum()
+        deaths_or_rate_by_fips = deaths_or_rate_by_fips.sort_values()
+        # Only look at non-zero rows:
+        deaths_or_rate_by_fips = deaths_or_rate_by_fips[deaths_or_rate_by_fips > 0]
+        fig = deaths_or_rate_by_fips.iplot(
+            kind="bar", y=AGGREGATE_BY, title=title, asFigure=True
+        )
+
+        fig_layout = fig["layout"]
+        fig_data = fig["data"]
+
+        fig_data[0]["text"] = deaths_or_rate_by_fips.values.tolist()
+        fig_data[0]["marker"]["color"] = (227, 227, 227)
+        fig_data[0]["marker"]["opacity"] = 1
+        fig_data[0]["marker"]["line"]["width"] = 0
+        fig_data[0]["textposition"] = "outside"
+        fig_layout["paper_bgcolor"] = (227, 227, 227)
+        fig_layout["plot_bgcolor"] = (227, 227, 227)
+        fig_layout["font"]["color"] = "#2cfec1"
+        fig_layout["title"]["font"]["color"] = "#2cfec1"
+        fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
+        fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
+        fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
+        fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
+        fig_layout["margin"]["t"] = 75
+        fig_layout["margin"]["r"] = 50
+        fig_layout["margin"]["b"] = 100
+        fig_layout["margin"]["l"] = 50
+
+        return fig
+
+    fig = dff.iplot(
+        kind="area",
+        x="Year",
+        y="Age Adjusted Rate",
+        text="County",
+        categories="County",
+        colors=[
+            "#1b9e77",
+            "#d95f02",
+            "#7570b3",
+            "#e7298a",
+            "#66a61e",
+            "#e6ab02",
+            "#a6761d",
+            "#666666",
+            "#1b9e77",
+        ],
+        vline=[year],
+        asFigure=True,
+    )
+
+    for i, trace in enumerate(fig["data"]):
+        trace["mode"] = "lines+markers"
+        trace["marker"]["size"] = 4
+        trace["marker"]["line"]["width"] = 1
+        trace["type"] = "scatter"
+        for prop in trace:
+            fig["data"][i][prop] = trace[prop]
+
+    # Only show first 500 lines
+    fig["data"] = fig["data"][0:500]
+
+    fig_layout = fig["layout"]
+
+    # See plot.ly/python/reference
+    # fig_layout["yaxis"]["title"] = "Age-adjusted death rate per county per year"
+    fig_layout["xaxis"]["title"] = ""
+    fig_layout["yaxis"]["fixedrange"] = True
+    fig_layout["xaxis"]["fixedrange"] = False
+    fig_layout["hovermode"] = "closest"
+    fig_layout["title"] = "<b>{0}</b> counties selected".format(len(fips))
+    fig_layout["legend"] = dict(orientation="v")
+    fig_layout["autosize"] = True
+    fig_layout["paper_bgcolor"] = "#1f2630"
+    fig_layout["plot_bgcolor"] = "#1f2630"
+    fig_layout["font"]["color"] = "#2cfec1"
+    fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
+    fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
+    fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
+    fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
+
+    if len(fips) > 500:
+        fig["layout"][
+            "title"
+        ] = "Age-adjusted death rate per county per year <br>(only 1st 500 shown)"
+
+    return fig
+
+
+if __name__ == "__main__":
     app.run_server(debug=True)
