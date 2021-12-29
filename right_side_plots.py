1adc5cf020140e19e8c87a2c57579411a2fb96c1
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-12-29 16:56:28 +0100

    A bit more refactoring

--- right_side_plots.py
+++ right_side_plots.py
@@ -30,38 +30,45 @@ def initilize_right_side_functionality(app, df_full_data):
         dff = dff.sort_values("accident_year")
 
         if chart_dropdown == "show_accidents_per_age":
-            title = "Accidents per age, <b>2016-2020</b>"
-            AGGREGATE_BY = "age_of_driver"
+            return accidents_per_age(dff)
 
-            dff[AGGREGATE_BY] = pd.to_numeric(dff[AGGREGATE_BY], errors="coerce")
-            dff = dff[["accident_index", AGGREGATE_BY]]
-            rate_per_age = dff.groupby(AGGREGATE_BY, as_index=False).count()
-            rate_per_age.rename(columns={"accident_index": "count"}, inplace=True)
-            rate_per_age.reset_index()
-            rate_per_age.drop([0], inplace=True)
-            fig = rate_per_age.iplot(
-                kind="bar", y='count', title=title, asFigure=True
-            )
 
-            fig_layout = fig["layout"]
-            fig_data = fig["data"]
+def accidents_per_age(dff):
+    """
+    This function generates the right side figure displaying accidents per age
+    """
+    title = "Accidents per age, <b>2016-2020</b>"
+    AGGREGATE_BY = "age_of_driver"
 
-            fig_data[0]["text"] = rate_per_age.values.tolist()
-            fig_data[0]["marker"]["color"] = (227, 227, 227)
-            fig_data[0]["marker"]["opacity"] = 1
-            fig_data[0]["marker"]["line"]["width"] = 0
-            fig_data[0]["textposition"] = "outside"
-            fig_layout["paper_bgcolor"] = "#e3e3e3"
-            fig_layout["plot_bgcolor"] = "#e3e3e3"
-            fig_layout["font"]["color"] = "#000000"
-            fig_layout["title"]["font"]["color"] = "#000000"
-            fig_layout["xaxis"]["tickfont"]["color"] = "#000000"
-            fig_layout["yaxis"]["tickfont"]["color"] = "#000000"
-            fig_layout["xaxis"]["gridcolor"] = "#787878"
-            fig_layout["yaxis"]["gridcolor"] = "#787878"
-            fig_layout["margin"]["t"] = 75
-            fig_layout["margin"]["r"] = 50
-            fig_layout["margin"]["b"] = 100
-            fig_layout["margin"]["l"] = 50
+    dff[AGGREGATE_BY] = pd.to_numeric(dff[AGGREGATE_BY], errors="coerce")
+    dff = dff[["accident_index", AGGREGATE_BY]]
+    rate_per_age = dff.groupby(AGGREGATE_BY, as_index=False).count()
+    rate_per_age.rename(columns={"accident_index": "count"}, inplace=True)
+    rate_per_age.reset_index()
+    rate_per_age.drop([0], inplace=True)
+    fig = rate_per_age.iplot(
+        kind="bar", y='count', title=title, asFigure=True
+    )
 
-            return fig
+    fig_layout = fig["layout"]
+    fig_data = fig["data"]
+
+    fig_data[0]["text"] = rate_per_age.values.tolist()
+    fig_data[0]["marker"]["color"] = (227, 227, 227)
+    fig_data[0]["marker"]["opacity"] = 1
+    fig_data[0]["marker"]["line"]["width"] = 0
+    fig_data[0]["textposition"] = "outside"
+    fig_layout["paper_bgcolor"] = "#e3e3e3"
+    fig_layout["plot_bgcolor"] = "#e3e3e3"
+    fig_layout["font"]["color"] = "#000000"
+    fig_layout["title"]["font"]["color"] = "#000000"
+    fig_layout["xaxis"]["tickfont"]["color"] = "#000000"
+    fig_layout["yaxis"]["tickfont"]["color"] = "#000000"
+    fig_layout["xaxis"]["gridcolor"] = "#787878"
+    fig_layout["yaxis"]["gridcolor"] = "#787878"
+    fig_layout["margin"]["t"] = 75
+    fig_layout["margin"]["r"] = 50
+    fig_layout["margin"]["b"] = 100
+    fig_layout["margin"]["l"] = 50
+
+    return fig
